4a82475f4d239ef313a42e25b123dc65a3799f06
Izora Kayumova <izorakayumova@Izoras-MacBook-Pro.local>
2023-09-15 14:39:10 -0400

    refactored to extract helper method

--- src/FizzBuzz.java
+++ src/FizzBuzz.java
@@ -37,34 +37,40 @@ class FizzBuzz {
     }
 }
 
-class FizzBuzz1 {
+class FizzBuzz_while {
 
     public static void main(String[] args) {
         int i = 1;
-        while (i < 100){
+        while (i < 100) {
 
-            // Find out which numbers divide i.
-            boolean divisibleBy3 = i % 3 == 0;
-            boolean divisibleBy5 = i % 5 == 0;
+            i = doFizzBuzz_while(i);
+        }
+    }
 
-            // Print our appropriate result.
-            if (divisibleBy3 && divisibleBy5) {
+    private static int doFizzBuzz_while(int i) {
+        // Find out which numbers divide i.
+        boolean divisibleBy3 = i % 3 == 0;
+        boolean divisibleBy5 = i % 5 == 0;
 
-                System.out.println("Fizz Buzz");
+        // Print our appropriate result.
+        if (divisibleBy3 && divisibleBy5) {
 
-            } else if (divisibleBy3) {
+            System.out.println("Fizz Buzz");
 
-                System.out.println("Fizz");
+        } else if (divisibleBy3) {
 
-            } else if (divisibleBy5) {
+            System.out.println("Fizz");
 
-                System.out.println("Buzz");
+        } else if (divisibleBy5) {
 
-            } else {
+            System.out.println("Buzz");
 
-                System.out.println(i);
+        } else {
 
-            }
-        i++;}
+            System.out.println(i);
+
+        }
+        i++;
+        return i;
     }
 }
